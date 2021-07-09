13dfee0ffdcd8160eb19db022174d436631012ba
Dmitriy Alhimovich <dmitriy.alhimovich@arcadia.spb.ru>
2021-07-09 12:33:03 +0300

    Operations Addition and Subtraction implemented

--- CalculatorWithRebase/Program.cs
+++ CalculatorWithRebase/Program.cs
@@ -6,7 +6,33 @@ namespace CalculatorWithRebase
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Input number:");
+            var number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input operation (+, -):");
+
+            var operation = Console.ReadLine()[0];
+
+            Console.WriteLine("Input number:");
+
+            var number2 = int.Parse(Console.ReadLine());
+
+            double result;
+
+
+            switch (operation)
+            {
+                case '+':
+                    result = number1 + number2;
+                    break;
+                case '-':
+                    result = number1 - number2;
+                    break;
+                default:
+                    throw new ArgumentException($"Operation not supported: {operation}");
+            }
+
+            Console.WriteLine($"Result: {result}");
         }
     }
 }
