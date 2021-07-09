bc8ff5eff9a3cbaa063476a6624dbddf56962eae
Dmitriy Alhimovich <dmitriy.alhimovich@arcadia.spb.ru>
2021-07-09 12:34:41 +0300

    Feature/Multiplication: Operation added

--- CalculatorWithRebase/Program.cs
+++ CalculatorWithRebase/Program.cs
@@ -9,7 +9,7 @@ namespace CalculatorWithRebase
             Console.WriteLine("Input number:");
             var number1 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Input operation (+, -):");
+            Console.WriteLine("Input operation (+, -, *):");
 
             var operation = Console.ReadLine()[0];
 
@@ -27,6 +27,9 @@ namespace CalculatorWithRebase
                     break;
                 case '-':
                     result = number1 - number2;
+                    break;
+                case '*':
+                    result = number1 * number2;
                     break;
                 default:
                     throw new ArgumentException($"Operation not supported: {operation}");
