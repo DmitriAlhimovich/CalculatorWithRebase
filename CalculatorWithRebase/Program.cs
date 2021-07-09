13e8bf7add68904a4003dbb8843a8f503aedbb83
Dmitriy Alhimovich <dmitriy.alhimovich@arcadia.spb.ru>
2021-07-09 12:31:00 +0300

    Add project files.

---
+++ CalculatorWithRebase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculatorWithRebase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
